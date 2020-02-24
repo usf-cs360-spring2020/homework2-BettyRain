dadc9328da0ec522bf10dde10492d5022a7a86f9
Betty <liza.ozerova@list.ru>
2020-02-23 22:27:08 -0800

    Added chart, still working on it

--- coord.js
+++ coord.js
@@ -1,67 +1,83 @@
-// location of data file
-let csv = 'data.csv';
+function drawParallelCoord(data) {
 
-// function to convert into date
-// try: parseRowDate('197912');
-let parseRowDate = d3.timeParse('%Y%m');
+  let config = {
+    'svg': {},
+    'margin': {},
+    'plot': {}
+  };
 
-// configuration of svg/plot area
-let config = {
-  'svg': {},
-  'margin': {},
-  'plot': {}
-};
+  config.svg.height = 470;
+  config.svg.width = 950;
 
-config.svg.height = 450;
-config.svg.width = config.svg.height * 1.618; // golden ratio
+  config.margin.top = 20;
+  config.margin.right = 10;
+  config.margin.bottom = 30;
+  config.margin.left = 10;
 
-config.margin.top = 10;
-config.margin.right = 10;
-config.margin.bottom = 30;
-config.margin.left = 10;
-
-config.plot.x = config.margin.left;
-config.plot.y = config.margin.top;
-config.plot.width = config.svg.width - config.margin.left - config.margin.right;
-config.plot.height = config.svg.height - config.margin.top - config.margin.bottom;
-
-// setup svg
-let svg = d3.select('body').select('#coord-chart');
-svg.attr('width', config.svg.width);
-svg.attr('height', config.svg.height);
+  let svg = d3.select('body').select('#coord');
+  svg.attr('width', config.svg.width);
+  svg.attr('height', config.svg.height + config.margin.bottom);
 
 
-d3.csv(csv, convertRow).then(drawChart);
+  dimensions = ["par_mean", "k_mean", "female", "ktop1pc_cond_parq1", "ktop1pc_cond_parq5"];
+  let color = d3.scaleOrdinal()
+    .domain(["1", "2", "3"])
+    .range(["#111f9e", "#23bd16", "#eb0d0d"]);
 
-// function to convert each row
-// we need sum of passengers && activity period in days
-// https://github.com/d3/d3-fetch/blob/master/README.md#csv
-function convertRow(row, index) {
-  let out = {};
-  out.date = new Date()
-  out.num = 0;
 
-  for (let col in row) {
-    switch (col) {
-      case 'Activity Period':
-        out.date = parseRowDate(row[col]);
-        break;
-
-      case 'Passenger Count':
-        out.num = parseInt(row[col]);
-        break;
-    }
+  let yAxis = {}
+  for (i in dimensions) {
+    name = dimensions[i]
+    yAxis[name] = d3.scaleLinear()
+      .domain(d3.extent(data, function(d) { return +d[name];  }))
+      .range([config.svg.height, config.margin.top])
   }
-  return out;
-}
 
-function drawChart(inputData) {
-  //https://www.d3-graph-gallery.com/graph/parallel_basic.html
+  let xAxis = d3.scalePoint()
+    .range([0, config.svg.width])
+    .padding(0.25)
+    .domain(dimensions);
 
-}
+  function path(d) {
+    return d3.line()(dimensions.map(function(p) {
+      return [xAxis(p), yAxis[p](d[p])];
+    }));
+  }
 
+  svg
+    .selectAll("lines")
+    .data(data)
+    .enter().append("path")
+    .attr("d", path)
+    .style("fill", "none")
+    .style("stroke", function(d) {
+      return (color(d.iclevel))
+    })
+    .style("opacity", 0.5)
+    .attr("transform", function(d) {
+      return translate(0, config.margin.top);
+    })
 
-// helper method to make translating easier
-function translate(x, y) {
-  return 'translate(' + x + ',' + y + ')';
+  svg.selectAll("x-axis")
+    .data(dimensions).enter()
+    .append("g")
+    .style("stroke", "black")
+    .attr("transform", function(d) {
+      return translate(xAxis(d), config.margin.top);
+    })
+    .style("fill", "none")
+    .style("opacity", 0.6)
+    .each(function(d) {
+      d3.select(this).call(d3.axisLeft().scale(yAxis[d]));
+    })
+
+    .append("text")
+      .style("text-anchor", "middle")
+      .attr("y", -1)
+      .text(function(d) {return d;});
+
+    // helper method to make translating easier
+    function translate(x, y) {
+      return 'translate(' + x + ',' + y + ')';
+    }
 }
